a3a1e7179d45929567be7bfac4d7f2b558704c8c
Jaziel Menezes <62656128+jazielciro@users.noreply.github.com>
2020-03-27 18:33:09 -0300

    Front-end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,16 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors);
+app.use(cors({
+  origin: 'http://localhost:3000'
+    }));
 app.use(express.json());
 app.use(routes);
 
+
+ 
 app.listen(3333);
+
 
 /**
  * Rotas / recurso
